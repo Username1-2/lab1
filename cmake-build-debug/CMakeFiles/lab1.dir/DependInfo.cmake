653388fc314598af7958cdb7cd38fafca86a323a
Username1-2 <76197053+Username1-2@users.noreply.github.com>
2020-12-23 11:34:06 +0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/lab1.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/name/learn/EC/labs/lab1/main.cpp" "/home/name/learn/EC/labs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/main.cpp.o"
+  "/home/name/learn/EC/labs/lab1/tests/tests.cpp" "/home/name/learn/EC/labs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/tests/tests.cpp.o"
+  "/home/name/learn/EC/labs/lab1/tritset.cpp" "/home/name/learn/EC/labs/lab1/cmake-build-debug/CMakeFiles/lab1.dir/tritset.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/googletest-master/googlemock/include"
+  "../lib/googletest-master/googletest/include"
+  "../lib/googletest-master/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/name/learn/EC/labs/lab1/cmake-build-debug/lib/googletest-master/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/name/learn/EC/labs/lab1/cmake-build-debug/lib/googletest-master/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
